db0fa4dd5ba8fd1149acbfd78e223351523052e1
Sean Kaufman <sean.kaufman@voxmedia.com>
2018-08-02 16:04:22 -0400

    adds docker recipe

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'cookbook-stackstorm-docker'
+maintainer       'Sean Kaufman'
+license          'All rights reserved'
+description      'Installs/Configures stackstorm via docker'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+depends 'docker'
+depends 'chef-yum-docker'
+depends 'git'
---
+++ recipes/default.rb
@@ -0,0 +1,36 @@
+###
+# Install docker-engine , configure as service and configure container
+include_recipe 'chef-yum-docker'
+
+docker_service 'default' do
+  action [:create, :start]
+end
+
+# docker-compose
+%w(python epel-release python-pip git).each do |package|
+  yum_package package do
+    action :upgrade
+  end
+end
+
+execute 'Install docker-compose' do
+  command 'pip install docker-compose'
+end
+
+directory '/opt/stacktorm/docker' do
+  recursive true
+end
+
+include_recipe 'git'
+
+git '/opt/stacktorm/docker/st2-docker' do
+  repository 'git@github.com:stackstorm/st2-docker'
+  reference 'master'
+  action :sync
+end
+
+execute 'start compose stack' do
+  command 'make env && docker-compose up -d'
+  cwd '/opt/stacktorm/docker/st2-docker'
+  not_if 'docker ps | grep st2'
+end
