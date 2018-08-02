9df39a37b67cb4143ece840f98dca24ebc3f8f6e
Sean Kaufman <sean.kaufman@voxmedia.com>
2018-08-02 16:19:40 -0400

    bump metadata

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Sean Kaufman'
 license          'All rights reserved'
 description      'Installs/Configures stackstorm via docker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 depends 'docker'
 depends 'chef-yum-docker'
